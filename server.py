2058ee8bad7e33b7ce6b92b06f73911ad3c278e3
Davis Liu <davisliu2006@gmail.com>
2025-01-18 15:29:29 -0500

    Fix server

--- server.py
+++ server.py
@@ -3,7 +3,12 @@ import flask
 app = flask.Flask(__name__)
 
 @app.route("/")
-def index():
+@app.route("/index.html")
+def site_index():
     return flask.render_template("index.html")
+
+@app.route("/app.html")
+def site_app():
+    return flask.render_template("app.html")
 
 app.run(port=3000)
