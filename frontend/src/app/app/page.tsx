4db73e38e4ff63d93ac8ba4d07123a6ed241d406
Evan Zhao <57083990+Evang264@users.noreply.github.com>
2025-01-19 03:56:47 -0500

    Fetch live vulnerabilities

--- frontend/src/app/app/page.tsx
+++ frontend/src/app/app/page.tsx
@@ -1,10 +1,25 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 
 export default function App() {
   const [url, setUrl] = useState("");
+  const [progresses, setProgresses] = useState(["", "", "", ""]);
+  const [fixes, setFixes] = useState(["", "", "", ""]);
+
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      const response = await fetch("http://127.0.0.1:3001/status");
+      const result = await response.json();
+      setProgresses(result.filter((x: { status: string }) => x.status));
+      setFixes(
+        result.filter((x: { recommendations: string }) => x.recommendations)
+      );
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <>
@@ -73,26 +88,61 @@ export default function App() {
             <tr>
               <th>#</th>
               <th>Issue</th>
-              <th>File</th>
-              <th>Details</th>
+              <th>Description</th>
+              <th>Recommended Fix</th>
+              <th>Progress</th>
             </tr>
             <tr>
               <td className="td-right">1</td>
-              <td>Insecure Password</td>
-              <td>https://www.site.com/login</td>
+              <td>SQL Injection</td>
               <td>
-                Password sent as plain text
-                <pre>insert_code();</pre>
+                In computing, SQL injection is a code injection technique used
+                to attack data-driven applications, in which malicious SQL
+                statements are inserted into an entry field for execution (e.g.
+                to dump the database contents to the attacker). SQL injection
+                must exploit a security vulnerability in an application's
+                software, for example, when user input is either incorrectly
+                filtered for string literal escape characters embedded in SQL
+                statements or user input is not strongly typed and unexpectedly
+                executed.
               </td>
+              <td>{fixes[0]}</td>
+              <td>{progresses[0]}</td>
             </tr>
             <tr>
               <td className="td-right">2</td>
-              <td>SQL Injection</td>
-              <td>https://www.site.com/login</td>
+              <td>Cross-site Scripting (XSS)</td>
               <td>
-                SQL injection possible
-                <pre>insert_code();</pre>
+                Cross-Site Scripting (XSS) attacks are a type of injection, in
+                which malicious scripts are injected into otherwise benign and
+                trusted websites. XSS attacks occur when an attacker uses a web
+                application to send malicious code, generally in the form of a
+                browser side script, to a different end user.
               </td>
+              <td>{fixes[1]}</td>
+              <td>{progresses[1]}</td>
+            </tr>
+            <tr>
+              <td className="td-right">1</td>
+              <td>Brute-force attack</td>
+              <td>
+                A brute-force attack consists of an attacker submitting many
+                passwords or passphrases with the hope of eventually guessing
+                correctly.
+              </td>
+              <td>{fixes[2]}</td>
+              <td>{progresses[2]}</td>
+            </tr>
+            <tr>
+              <td className="td-right">1</td>
+              <td>Exposed Directories</td>
+              <td>
+                An exposed directories vulnerability is a security flaw in a web
+                server that allows unauthorized users to view the contents of
+                directories on a website.
+              </td>
+              <td>{fixes[3]}</td>
+              <td>{progresses[3]}</td>
             </tr>
           </table>
         </div>
